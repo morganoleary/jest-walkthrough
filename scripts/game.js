ef50bf018c1960eac326b9fce3e8adcd975086ea
Morgan Oleary <morganleeoleary@gmail.com>
2024-01-09 17:50:42 +0000

    Added video tutorial code through new game function

---
+++ scripts/game.js
@@ -0,0 +1,19 @@
+let game = {
+    currentGame: [],
+    score: 0,
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+};
+
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore };
